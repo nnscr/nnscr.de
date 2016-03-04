2c755b3d268b655d5346bfda96e378cb09368b2b
nnscr <info@nnscr.de>
2016-03-04 16:08:34 +0100

    bootstrap dependency, markdown editor fix ctrl+tab

--- pages/admin.py
+++ pages/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django import forms
 from . import models
+from nnmarkdown.form import MarkdownWidget
 from nnscr.admin import site
 
 
+class PageAdminForm(forms.ModelForm):
+    class Meta:
+        model = models.Page
+        exclude = ("slug",)
+        widgets = {
+            "text": MarkdownWidget
+        }
+
+
 class PageAdmin(admin.ModelAdmin):
-    fields = ["title", "text"]
+    form = PageAdminForm
+
 
 site.register(models.Page, PageAdmin)
